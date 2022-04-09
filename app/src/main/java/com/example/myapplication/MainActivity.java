2c9a22a74eecbd97c0e60e1d6f41aba49bd68373
jyp9i7y <jyp9i7y@gmail.com>
2022-04-09 12:50:49 +0900

    test commit 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                int a =1;
             }
         });
     }
